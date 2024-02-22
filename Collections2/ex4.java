5779a1f0ec378f5fda94e25f0dc1261562348b18
GS-Dio <149898417+GS-Dio@users.noreply.github.com>
2024-02-22 20:02:39 +0600

    Задачка4

--- Collections2/ex4.java
+++ Collections2/ex4.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ex3 {
+public class ex4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> colors = new ArrayList<>();
 
         colors.add("red");
         colors.add("blue");
+        colors.add("green");
+        colors.add("yellow");
 
-        System.out.println(colors);
-
-        System.out.print("Изменить первый элемент на: ");
-        colors.set(0,scanner.nextLine());
-
-        System.out.println(colors);
+        System.out.print("Какой по счету элемент вывести: ");
+        int index = scanner.nextInt() - 1;
+        System.out.println(colors.get(index));
     }
 }
