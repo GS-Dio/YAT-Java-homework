925e650c511504abf73ff61d454aa45ebaa0e74c
GS-Dio <149898417+GS-Dio@users.noreply.github.com>
2024-02-22 19:56:29 +0600

    Update ex3.java

--- Collections2/ex4.java
+++ Collections2/ex4.java
@@ -1,4 +1,22 @@
 package Collections2;
 
-public class ex4 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> colors = new ArrayList<>();
+
+        colors.add("red");
+        colors.add("blue");
+
+        System.out.println(colors);
+
+        System.out.print("Изменить первый элемент на: ");
+        colors.set(0,scanner.nextLine());
+
+        System.out.println(colors);
+    }
 }
